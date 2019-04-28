e40e62433d9293679225a6d84b0c29fe49d86434
Trevor <takettel@hotmail.com>
2019-04-27 20:50:09 -0500

    score check working version

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,27 @@
 // Actually have to SET the friends array, not just assume this will be a thing.  Remember SCOPE.
 let friendList = [
+    {
+        friendName: 'The Argent Adept, Virtuoso of the Void',
+        friendPic: 'https://sentinelswiki.com/images/2/24/Argentadeptisolated.png',
+        scores: [
+            2,4,5,5,4,2,4,5,2,4
+        ]
+    },
+    {
+        friendName: 'Tachyon, The Quickest Woman on Earth',
+        friendPic: 'https://sentinelswiki.com/images/a/af/Tachyonisolated.png',
+        scores: [
+            5,3,3,1,2,4,5,2,1,5
+        ]
+    },
+    {
+        friendName: 'The Naturalist',
+        friendPic: 'https://sentinelswiki.com/images/2/2b/Naturalistisolated.png',
+        scores: [
+            2,4,2,1,4,2,5,2,2,2
+        ]
+    }
+
 ]
 
 module.exports = friendList;
